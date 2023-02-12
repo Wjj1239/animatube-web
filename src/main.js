b59ae202f15fb46706121f5eab75083ff741408f
wjj <2307491719@qq.com>
2023-02-12 11:58:45 +0800

    全局设置

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+// import 'element-ui/lib/theme-chalk/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// 新增代码：引入全部组件及样式
+// import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import '@/assets/scss/index.scss'
+
+const app = createApp(App)
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
